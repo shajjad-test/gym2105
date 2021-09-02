00713a755cca764f4ee74e3f1ddd155aa71f67f6
shajjad-test <shajjad.test123@gmail.com>
2021-09-02 19:58:47 +0600

    eam ends

--- js/custom.js
+++ js/custom.js
@@ -10,4 +10,11 @@ $(function(){
     $('.venobox').venobox({
         closeBackground: '#e23e38',
     });
+
+    //team slider 
+    $('.team-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+    });
 });
