7584fa3e1cc4680069b1ba799670155e8349484c
shajjad-test <shajjad.test123@gmail.com>
2021-09-02 18:27:17 +0600

    banner ends

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(function(){
+    //banner slider;
+    $('.banner-slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+    });
+
+    //about video pop up;
+    $('.venobox').venobox({
+        closeColor: '#e23e38',
+    });
+});
